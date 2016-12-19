0c5c421c8d61388beadf7c450a124aefbf1e3db3
Saji Bhaskaran <sajibhaskaran@outlook.com>
2016-12-18 22:00:55 -0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+
+
+console.log(screen.width);
+if(screen.width < 400){
+  $('#page-header h1').addClass("width");
+}
+
+/*var turn = '.page-header';
+
+$('.navbar-brand').click(function(){
+  $(turn).fadeOut(1000);
+  $('.page-header').fadeIn(1000);
+  turn = '.page-header';
+  //$('#contact').fadeIn(1000);
+});
+$('.contact').click(function(){
+ // $('.page-header').fadeIn(1000);
+  $(turn).removeClass("margin");
+  $('#contact').addClass("margin");
+  $(turn).fadeOut(1000);
+  $('#contact').fadeIn(1000);
+  turn = '#contact';
+});
+$('.about').click(function(){
+  $(turn).removeClass("margin");
+  $('#about').addClass(".margin");
+  $(turn).fadeOut(1000);
+  $('#about').fadeIn(1000);
+  turn = '#about';
+});
+$('.portfolio').click(function(){
+   $(turn).removeClass("margin");
+  $('#portfolio').addClass("margin");
+  $(turn).fadeOut(600);
+  $('#portfolio').fadeIn(1000);
+  turn = '#portfolio';
+});*/
